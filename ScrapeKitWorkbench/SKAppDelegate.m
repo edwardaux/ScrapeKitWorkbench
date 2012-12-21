9a593dc56dd2fc8e6bb116e5a61610fceaeb3418
Craig Edwards <craig@hae.com.au>
2012-12-21 16:28:21 +1100

    Initial Commit

---
+++ ScrapeKitWorkbench/SKAppDelegate.m
@@ -0,0 +1,18 @@
+//
+//  SKAppDelegate.m
+//  ScrapeKitWorkbench
+//
+//  Created by Craig Edwards on 21/12/12.
+//  Copyright (c) 2012 BlackDog Foundry. All rights reserved.
+//
+
+#import "SKAppDelegate.h"
+
+@implementation SKAppDelegate
+
+- (void)applicationDidFinishLaunching:(NSNotification *)aNotification
+{
+	// Insert code here to initialize your application
+}
+
+@end
---
+++ ScrapeKitWorkbench/main.m
@@ -0,0 +1,14 @@
+//
+//  main.m
+//  ScrapeKitWorkbench
+//
+//  Created by Craig Edwards on 21/12/12.
+//  Copyright (c) 2012 BlackDog Foundry. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+int main(int argc, char *argv[])
+{
+	return NSApplicationMain(argc, (const char **)argv);
+}
