0091cbafd62048f3290ec7d39a157b53f7018348
Craig Edwards <craig@hae.com.au>
2012-12-28 06:45:32 +1100

    A bunch more compilation testcases

--- ScrapeKitTestCases/Classes/SKTestCompilation.m
+++ ScrapeKitTestCases/Classes/SKTestCompilation.m
@@ -14,7 +14,6 @@
 
 @implementation SKTestCompilation
 
-
 -(void)testNoMain {
 	NSString *script = @""
 	@"@blah\n"
@@ -22,6 +21,66 @@
 	@"	:end";
 	
 	[self confirmInvalid:script expectedError:@"No main function"];
+}
+
+-(void)testMissingFunction {
+	NSString *script = @""
+	@"	goto :end\n"
+	@"	:end";
+	
+	[self confirmInvalid:script expectedError:@"Rule \"goto :end\" declared outside of a function"];
+}
+
+-(void)testEmptyFunction1 {
+	NSString *script = @""
+	@"@main\n";
+	
+	[self confirmValid:script];
+}
+
+-(void)testEmptyFunction2 {
+	NSString *script = @""
+	@"@main\n"
+	@"@foo\n"
+	@"	goto :end\n"
+	@"	:end";
+	
+	[self confirmValid:script];
+}
+
+-(void)testNoTrailingCR1 {
+	NSString *script = @""
+	@"@main";
+	
+	[self confirmValid:script];
+}
+
+-(void)testNoTrailingCR2 {
+	NSString *script = @""
+	@"@main\n"
+	@"	:end";
+	
+	[self confirmValid:script];
+}
+
+-(void)testTrailingCR {
+	NSString *script = @""
+	@"@main\n\n\n";
+	
+	[self confirmValid:script];
+}
+
+-(void)testWhitespace {
+	NSString *script = @""
+	@"@main\n"
+	@":end1\n"
+	@"\n"
+	@"\t\n"
+	@" \n"
+	@" :end2\n"
+	@"	:end3";
+	
+	[self confirmValid:script];
 }
 
 @end
